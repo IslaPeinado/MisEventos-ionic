7d214bfcc76cb752b0d0804a685d5bde83e3cbae
IslaP <i.peinadohenriquez@gmail.com>
2023-05-22 14:52:26 +0200

    Tsk-Componets add-evento, list-evento, one-evento

---
+++ src/app/Componets/one-evento/one-evento.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-one-evento',
+  templateUrl: './one-evento.component.html',
+  styleUrls: ['./one-evento.component.scss'],
+})
+export class OneEventoComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
