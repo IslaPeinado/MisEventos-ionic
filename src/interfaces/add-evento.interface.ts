6de6fd7ddeb1b3466e80813c01f1c2d1dfba08ca
IslaP <i.peinadohenriquez@gmail.com>
2023-05-22 15:50:08 +0200

    Tsk-Firebase-AddEvento interface

---
+++ src/interfaces/add-evento.interface.ts
@@ -0,0 +1,9 @@
+export default interface AddEventoInterface {
+    id: string;
+    imagen: string;
+    titulo: string;
+    descripcion: string;
+    lugar: string;
+    dia: string;
+    hora: string;
+}
